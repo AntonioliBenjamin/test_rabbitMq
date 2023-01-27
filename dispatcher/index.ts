ad30ed7f92a1ec2a9f5d2c2bd32226a0f1f173a9
Cédric Herouard <115704191+Bric66@users.noreply.github.com>
2023-01-27 17:23:10 +0100

    test ok

--- dispatcher/index.ts
+++ dispatcher/index.ts
@@ -1,110 +1,43 @@
 import "reflect-metadata"
 import express from 'express'
 import {DomainEvent} from "ddd-messaging-bus"
-import { EventHandler } from "ddd-messaging-bus"
-import {EventHandlerRegistry, EventReceiver, rabbitMqBuild, MessageIdentifiers} from "ddd-messaging-bus";
-import { Container } from "inversify";
-import { injectable } from "inversify";
-import {inject} from "inversify";
+import {rabbitMqBuild, MessageIdentifiers} from "ddd-messaging-bus";
+import {Container} from "inversify";
 import {EventDispatcher} from "ddd-messaging-bus"
 
 const app = express()
 const port = 3000
+const container = new Container()
+const urlAmqp = 'amqps://xdoesltn:K8V5ZjrCUlIhKwFi6UsyJcEcGZyi8HvT@rattlesnake.rmq.cloudamqp.com/xdoesltn'
 
-const urlAmqp ='amqps://xdoesltn:K8V5ZjrCUlIhKwFi6UsyJcEcGZyi8HvT@rattlesnake.rmq.cloudamqp.com/xdoesltn'
-
-//domain event ----------
 export type YourDomainEventProperties = {
     userName: string;
     email: string;
 }
 
-export class MyDomainEvent extends DomainEvent<YourDomainEventProperties > {
-    constructor(props: YourDomainEventProperties ) {
+export class MyDomainEvent extends DomainEvent<YourDomainEventProperties> {
+    constructor(props: YourDomainEventProperties) {
         super(props)
     }
 }
-//----------------------
 
-
-
-//event handler---------
-@injectable()
-export class MyEventHandler implements EventHandler {
-
-    async handle(domainEvent: MyDomainEvent): Promise<void> {
-        console.log(`this is my domain event : ${domainEvent}`)
-    }
-}
-//---------------------
-
-
-
-//Use case ------------
-type UseCaseInput = {
-  userName: string;
-  email: string
+async function init() {
+    await rabbitMqBuild(container, urlAmqp)
 }
 
-@injectable()
-export class UseCase {
-    constructor(@inject(MessageIdentifiers.EventDispatcher) private readonly eventDispatcher: EventDispatcher) 
-    {}
-    
-    async execute(input: UseCaseInput) {
-      const myDomainEvent = new MyDomainEvent({
-        email: input.email,
-        userName: input.userName
-      })
-
-       await this.eventDispatcher.dispatch(myDomainEvent) 
-  }
+async function dispatch() {
+    const myDomainEvent = new MyDomainEvent({
+        email: 'toto@gmail.com',
+        userName: 'toto'
+    })
+    const eventDispatcher = container.get<EventDispatcher>(MessageIdentifiers.EventDispatcher);
+    await eventDispatcher.dispatch(myDomainEvent)
+    console.log("dispatched")
 }
-//---------------------
 
-
-//identifier-----------
-const identifier = { UseCase : Symbol.for('UseCase')};
-//---------------------
-
-
-
-//container------------
-class MyContainer extends Container {
-	async init() {
-	
-		rabbitMqBuild(this, urlAmqp)
-
-    this.bind(identifier.UseCase).toSelf()
-
-  	EventHandlerRegistry.register(MyDomainEvent, new MyEventHandler());
-
-		const eventReceiver: EventReceiver = this.get(MessageIdentifiers.EventReceiver);
-
-		await eventReceiver.init()
-	}
-}
-//---------------------
-
-
-
-//Setup----------------
-const container = new MyContainer()
-container.init()
-//---------------------
-
-
-
-const eventDispatcher: EventDispatcher = container.get(MessageIdentifiers.EventDispatcher)
-
-const useCase = new UseCase(eventDispatcher)
-
-useCase.execute({
-  email: 'toto',
-  userName: 'toto'
-})
+init().then(r => dispatch())
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
--- receiver/index.ts
+++ receiver/index.ts
@@ -1,9 +1,54 @@
 import "reflect-metadata"
 import express from 'express'
+import {
+    DomainEvent,
+    EventHandler,
+    EventHandlerRegistry,
+    MessageIdentifiers,
+    rabbitMqBuild,
+    EventReceiver
+} from "ddd-messaging-bus";
+import {Container} from "inversify";
+
 const app = express()
 const port = 4000
 
+const container = new Container()
+const urlAmqp = 'amqps://xdoesltn:K8V5ZjrCUlIhKwFi6UsyJcEcGZyi8HvT@rattlesnake.rmq.cloudamqp.com/xdoesltn'
+
+export type YourDomainEventProperties = {
+    userName: string;
+    email: string;
+}
+
+export class MyDomainEvent extends DomainEvent<YourDomainEventProperties> {
+    constructor(props: YourDomainEventProperties) {
+        super(props)
+    }
+}
+
+class MyDomainEventHandler implements EventHandler {
+
+    async handle(domainEvent: MyDomainEvent): Promise<void> {
+        console.log(domainEvent);
+        return
+    }
+}
+
+async function init() {
+    await rabbitMqBuild(container, urlAmqp);
+    EventHandlerRegistry.register(MyDomainEvent, new MyDomainEventHandler());
+
+    const eventReceiver = container.get<EventReceiver>(MessageIdentifiers.EventReceiver);
+
+    await eventReceiver.init();
+
+    console.log("Event successfully handled")
+
+}
+
+init()
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
